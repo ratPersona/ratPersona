e17dbc1871abd18b50238cd67a2de5d980ff791d
Sam Nelson <sam.nelson@mecinc.com>
2020-10-06 20:10:34 -0500

    added files for user experience project

---
+++ src/components/projects/LWUserTypes.vue
@@ -0,0 +1,19 @@
+<style lang="scss">
+</style>
+
+<template>
+  <main>
+
+
+  </main>
+</template>
+
+<script>
+export default {
+  data: function() {
+    return {
+      
+    }
+  },
+}
+</script>
