28ff40004050315642d1fb2655f6d184ac7ee74c
Sammi Nelson <ratpersona@Sammis-MacBook-Air.local>
2021-03-14 09:30:07 -0500

    working on nomader info

--- src/components/portfolio/Nomader.vue
+++ src/components/portfolio/Nomader.vue
@@ -10,21 +10,71 @@
         <div class="study-media">
           <img class="nomader-one" src="@/assets/portfolio/ux/nomadermu1.png">
         </div>
-        <div class="study-info">
+        <div class="study-info name-info">
           <div class="inner">
-            <p>Road trips provide a lot of things, one of the most important being memories. Regardless of the crew whether alone, with family, or just a pair of pals; the memories are what everyone appreciates from their nomadic experience. Nomader is an app that produces and saves these neato moments in a colorful way!</p>
-            <p>Other road trip apps that plan or are simply a mapping system provide their own unique experience, but they felt impersonal. Not only that, but they are somewhat limited. Trip curation takes a lot of time, and through Nomader we wanted to give users the feeling that the app recognized them as an individual. Nomader considers the user’s interests through quizzes, algorithm updates, and selections the user makes. The purpose of this is to suggest places for the user to visit or even entire pre-planned trips if they’re feeling spontaneous. It’s a step in the ever-progressing direction of smart devices and their reliability.</p>
+            <p>Road trips provide a lot of things, one of the most important being memories. Whether alone, with family, or just a pair of pals; the memories and experiences are the greatest takeaway. Nomader is an app that produces and saves these neato moments in a colorful way!</p>
+            <p></p>
           </div>
         </div>
       </div>
       <div v-else-if="activeButton == 1" class="study-each research">
         <div class="study-info research-info">
-          <div class="inner">
-            <h2>What are we looking at?</h2>
-            <p>Nomader stemmed from a couple basic thoughts that were molded into a HMW statement:</p>
-            <p>How might we design an innovative experience for a crowded traveling app market and reduce the stress of travel planning?</p>
-            <p>To solve this, market research and user interest was performed.</p>
-            <div>
+          <div class="research-upper">
+            <div v-if="activeCard == 1" class="info-card one" :class="{active : activeCard == 1}">
+              <h2>What are we looking at?</h2>
+              <p>Nomader stemmed from a couple basic thoughts that were molded into a HMW statement:</p>
+              <p>How might we design an innovative experience for a crowded traveling app market and reduce the stress of travel planning?</p>
+              <p>To solve this, market research and user interest was performed.</p>
+            </div>
+            <div v-else-if="activeCard == 2"  class="info-card two" :class="{active : activeCard == 2}">
+              <h2>What are we looking at?</h2>
+              <p>Nomader stemmed from a couple basic thoughts that were molded into a HMW statement:</p>
+              <p>How might we design an innovative experience for a crowded traveling app market and reduce the stress of travel planning?</p>
+              <p>To solve this, market research and user interest was performed.</p>
+            </div>
+            <div v-else-if="activeCard == 3" class="info-card three" :class="{active : activeCard == 3}">
+              <h2>What are we looking at?</h2>
+              <p>Nomader stemmed from a couple basic thoughts that were molded into a HMW statement:</p>
+              <p>How might we design an innovative experience for a crowded traveling app market and reduce the stress of travel planning?</p>
+              <p>To solve this, market research and user interest was performed.</p>
+            </div>
+            <div v-else-if="activeCard == 4" class="info-card four" :class="{active : activeCard == 4}">
+              <h2>What are we looking at?</h2>
+              <p>Nomader stemmed from a couple basic thoughts that were molded into a HMW statement:</p>
+              <p>How might we design an innovative experience for a crowded traveling app market and reduce the stress of travel planning?</p>
+              <p>To solve this, market research and user interest was performed.</p>
+            </div>
+          </div>
+          <div class="flex-container">
+            <div class="inner card one" :class="{active : activeCard == 1}">
+              <h3>Who</h3>
+              <ul class="white-list">
+                <li></li>
+              </ul>
+              <button>More</button>
+            </div>
+            <div class="inner card two" :class="{active : activeCard == 2}">
+              <h3>How</h3>
+              <ul class="white-list">
+                <li></li>
+              </ul>
+              <button>More</button>
+            </div>
+            <div class="inner card three" :class="{active : activeCard == 3}">
+              <h3>Heading</h3>
+              <ul class="white-list">
+                <li></li>
+              </ul>
+              <button>More</button>
+            </div>
+            <div class="inner card four" :class="{active : activeCard == 4}">
+              <h3>Heading</h3>
+              <ul class="white-list">
+                <li></li>
+              </ul>
+              <button>More</button>
+            </div>
+            <!-- <div class="inner">
               <h3>Who is our target?</h3>
               <p>After creating proto-personas, people who could be potential users were interviewed. Based on the response from these interviews, Nomader had a good potential target user base, including:</p>
               <ul class="green-list">
@@ -33,21 +83,16 @@
                 <li><span>People who would like to start travelling.</span></li>
               </ul>
             </div>
-            <div>
+            <div class="inner">
               <h3>What can we provide?</h3>
-              <p></p>
               <ul class="green-list">
                 <li><span>djsafl</span></li>
                 <li><span>fsdf</span></li>
                 <li><span>fsd;kf</span></li>
               </ul>
-            </div>
+            </div> -->
           </div>
         </div>
-        <!-- <img class="nomader-two map" src="@/assets/portfolio/ux/map.png">
-        <img class="nomader-two person one" src="@/assets/portfolio/ux/user.png">
-        <img class="nomader-two person two" src="@/assets/portfolio/ux/user.png">
-        <img class="nomader-two person three" src="@/assets/portfolio/ux/user.png"> -->
       </div>
       <div v-else-if="activeButton == 2" class="study-each method">
         <img class="nomader-three" src="@/assets/portfolio/ux/sketchflow.png">
@@ -92,8 +137,8 @@ export default {
   data: function() {
     return {
       activeItem: '',
-      activeButton: 0
-      // title: 'Nomader'
+      activeButton: 0,
+      activeCard: 1
     }
   },
   computed: {
